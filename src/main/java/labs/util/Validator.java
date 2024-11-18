a2c38f0cfcd6b6dde7c23abf81e871466166aad8
krevetkot <ksenia.slonimskaya@yandex.ru>
2024-11-18 23:03:15 +0300

    draw area with dots!

--- src/main/java/labs/util/Validator.java
+++ src/main/java/labs/util/Validator.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class Validator {
 
     public boolean isHit(float x, float y, float r) {
-        return ((x >= 0) && (x <= r/2) && (y >= 0) && (y <= r) || //in rectangle
-                (x>=0) && (y >= x * r/2) && (y <= 0) || //in triangle
-                (x * x + y * y <= (r/2) * (r/2)) && (x <= 0) && (y <= 0) //in circle
+        return ((x >= 0) && (x <= r) && (y >= 0) && (y <= r) || //in rectangle
+                (x>=0) && (y >= x * r/2 - (r/2)) && (y <= 0) || //in triangle
+                (x * x + y * y <= r * r ) && (x <= 0) && (y <= 0) //in circle
         );
     }
 
--- src/main/webapp/js/area.js
+++ src/main/webapp/js/area.js
@@ -42,9 +42,9 @@ function sendPoint(x, y, r) {
 
 function isItHit(x, y, r){
     x = parseFloat(x);
-    return ((x >= 0) && (x <= r/2) && (y >= 0) && (y <= r) || //in rectangle
-        (x>=0) && (y >= x * r/2) && (y <= 0) || //in triangle
-        (x * x + y * y <= (r/2) * (r/2)) && (x <= 0) && (y <= 0) //in circle
+    return ((x >= 0) && (x <= r) && (y >= 0) && (y <= r) || //in rectangle
+        (x>=0) && (y >= x * r/2 - (r/2)) && (y <= 0) || //in triangle
+        (x * x + y * y <= r * r ) && (x <= 0) && (y <= 0) //in circle
     );
 }
 
